f651c8a7c58a769d1a6f5c0c6b66df92189e6a6f
Aaron Briscoe <abriscoe2008gmail.com>
2019-09-04 09:15:56 -0500

    Done.

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -4,21 +4,87 @@ import PortfolioContainer from './PortfolioContainer'
 import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
+  constructor(){
+    super()
+
+    this.state={
+      stocks: [],
+      portfolioStocks: [],
+      displayStocks: []
+    }
+  }
+
+  componentDidMount(){
+    fetch('http://localhost:3000/stocks')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        stocks: data,
+        displayStocks: data
+      })
+    })
+  }
+
+  buyStock = (stock) => {
+    let arr = this.state.portfolioStocks
+    arr.push(stock)
+    this.setState({
+      portfolioStocks: arr
+    })
+    
+  }
+
+  sellStock = (stock) => {
+    let arr = this.state.portfolioStocks.filter(s => s !== stock)
+
+    this.setState({
+      portfolioStocks: arr
+    })
+  }
+
+  filterStocks = (type) => {
+    
+    if(type !== 'All') {this.setState({
+      displayStocks: this.state.stocks.filter(stock => stock.type === type)
+    })}
+    else { 
+       this.setState({
+        displayStocks: this.state.stocks
+      })
+    }
+    
+  }
+
+  sortStocks = (sortBy) => {
+    let arr = []
+    switch(sortBy){
+      case 'Alphabetically':
+        arr = this.state.displayStocks.sort((a,b) => a.name > b.name ? 1 : -1)
+        break;
+      case 'Price':
+          arr = this.state.displayStocks.sort((a,b) => a.price > b.price ? 1 : -1) 
+        break;
+      default:console.log('wrong choice')
+    }
+    this.setState({
+      displayStocks: arr
+    })
+  }
 
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar filterStocks={this.filterStocks} sortStocks={this.sortStocks}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.displayStocks} buyStock={this.buyStock}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={this.state.portfolioStocks} sellStock={this.sellStock}/>
 
             </div>
           </div>
